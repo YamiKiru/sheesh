4fd0cc1b06a7887aee0705139ca8e00571d1d338
YamiKiru <94007078+YamiKiru@users.noreply.github.com>
2021-11-16 18:35:19 +0000

    Add files via upload

---
+++ Poor-Jet-0/Poor-Jet-0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poor_Jet_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(1, 13); //Cria um nº entre 1 e 13.
+            int b = rnd.Next(1, 7); //Cria um nº entre 1 e 7.
+            int c = rnd.Next(52); //Cria um nº entre 0 e 52.
+            Console.WriteLine(a + " " + b + " " + c);
+            Console.ReadLine();
+        }
+    }
+}
