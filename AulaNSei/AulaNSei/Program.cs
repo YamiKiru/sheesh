21dd72347300479f5699059a144cde1464b52dbd
YamiKiru <94007078+YamiKiru@users.noreply.github.com>
2021-11-16 18:15:39 +0000

    Add files via upload

---
+++ AulaNSei/AulaNSei/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaNSei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi();
+            //SayHi2();
+            //SayHi2();
+            //SayHi3();
+            //SayHi3();
+
+            Console.ReadLine();
+        }
+        static void SayHi()
+        {
+            Console.Write("Hello User");
+        }
+        static void SayHi2(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+        static void SayHi3(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you're " + age);
+        }
+    }
+}
