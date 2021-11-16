8e221880018543ffe2fb114d4f771dc6f9d3371b
YamiKiru <94007078+YamiKiru@users.noreply.github.com>
2021-11-16 18:16:40 +0000

    Add files via upload

---
+++ aula6/aula6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckynumbers = { 4, 2, 5, 7, 9, 17 };
+            string[] player = new string[5]; //guarda 5 valores.
+
+            Console.WriteLine(luckynumbers[0]);
+
+            luckynumbers[1] = 900;
+
+            Console.WriteLine(luckynumbers[1]);
+
+            player[0] = "Kiru";
+            player[1] = "Kira";
+            player[2] = "Kiro";
+            player[3] = "Kiri";
+            player[4] = "Kire";
+
+            Console.WriteLine(player[4]);
+
+            Console.ReadLine();
+        }
+    }
+}
