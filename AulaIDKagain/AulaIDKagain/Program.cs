580b22b05a8489d3aa2b4b4deaa2f8a961879852
YamiKiru <94007078+YamiKiru@users.noreply.github.com>
2021-11-16 18:15:19 +0000

    Add files via upload

---
+++ AulaIDKagain/AulaIDKagain/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaIDKagain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int cubedNumber = cube2(6);
+            //Console.WriteLine(cubedNumber);
+            Console.WriteLine(cube(5));
+            Console.ReadLine();
+        }
+        static int cube(int num)
+        {
+            int result = num + num + num;
+            return result;
+        }
+        /*static int cube2(int num)
+        {
+            int result = num + num + num;
+            return result;
+        }*/
+    }
+}
